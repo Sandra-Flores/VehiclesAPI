eef6386ccb58c187d9a3e8c1407f6cb32a5c35d7
Sandra-Flores <sfloressilverio@csumb.edu>
2019-11-19 23:43:24 -0800

    Autowired

--- vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
@@ -2,6 +2,8 @@ package com.udacity.vehicles.client.prices;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -13,9 +15,11 @@ public class PriceClient {
 
     private static final Logger log = LoggerFactory.getLogger(PriceClient.class);
 
+
     private final WebClient client;
 
-    public PriceClient(WebClient pricing) {
+    @Autowired
+    public PriceClient(@Qualifier("pricing") WebClient pricing) {
         this.client = pricing;
     }
 
